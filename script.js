ead1ed5a929769f031a123da912aa46b2b6ac0e1
Sam Bassong <sam.bassong@gmail.com>
2021-08-10 17:09:30 -0700

    added a working resetJourney function

--- script.js
+++ script.js
@@ -12,19 +12,32 @@ let gameActive = false
 let dieVal = 0
 
 
-//Die function
+//throwDie function
 function throwDie () {
     dieVal = Math.ceil(Math.random() * 6)
 }
 
+//startJourney which handles getting the hero moving on the path
 function startJourney() {
-  
   if (dieVal === 6) {
     let child = base.removeChild(hero)
     p1.appendChild(child)
     heroPosition = p1.id
     gameActive = true
-  } else return
+    messageDiv.innerText = `You rolled the magic number ${dieVal} and can now start your journey. Good luck!`
+  } else {
+    messageDiv.innerText = `You rolled a ${dieVal}. Try again for the magic number!`
+  }
+  
+}
+
+//resetJourney which resets current journey to base
+function resetJourney() {
+  heroPosition = ''
+  gameActive = false
+  dieVal = 0
+  base.appendChild(hero)
+  messageDiv.innerText = ''
 }
 
 //movements
@@ -35,6 +48,7 @@ function moveForward(dieVal) {
   let child = curPosition.removeChild(hero)
   nextPosition.appendChild(child)
   heroPosition = nextPosition.id
+  messageDiv.innerText = `You move forward ${dieVal} tiles. Keep it up!`
 }
 //moveBack
 function moveBack() {
@@ -43,13 +57,17 @@ function moveBack() {
     let newPosition = document.getElementById(`${heroPosition - 3}`)
     newPosition.appendChild(child)
     heroPosition = newPosition.id
-    
+    messageDiv.innerText = `Uh oh, you rolled a trouble ${dieVal}. You move back ${dieVal} tiles. don't give up!`
+
   } else {
     let child = document.getElementById(heroPosition).removeChild(hero)
     base.appendChild(child)
     heroPosition = base.id
+    messageDiv.innerText = `Uh oh, you rolled a trouble ${dieVal}. You move back to base. Hang in there!`
   }
 }
+
+
 
 //actual movement logic handler
 function journey () {
@@ -63,16 +81,14 @@ function journey () {
     moveForward(dieVal)
     }
   }
+  // checkWin()
 }
 
 
 
 //event listeners
 startButton.addEventListener('click', function() {
-  heroPosition = ''
-  gameActive = false
-  dieVal = 0
-  base.appendChild(hero)
+  resetJourney()
 })
 
 
